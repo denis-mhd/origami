7df29dc0ba12974e263261742c2bf9a5d3a08e6c
denis <denis_mhd@yahoo.com>
2021-12-22 13:27:04 +0200

    add main container

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,38 @@
 import style from './App.module.css';
 import Header from './components/Header/Header';
+import Menu from './components/Menu/Menu';
+import Main from './components/Main/Main';
+import { Component } from 'react';
 
 
-function App() {
-  return (
-    <div className={style.app}>
-        <h1>Let's Begin</h1>
+// function App() {
+//   return (
+//     <div className={style.app}>
+//         <Header/>
+//         <div className={style.container}>
+//           <Menu/>
+
+//           <Main/>
+//         </div>
+        
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className={style.app}>
         <Header/>
+        <div className={style.container}>
+          <Menu/>
+
+          <Main/>
+        </div>
+        
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import './Main.css'
+import {Fragment} from 'react';
+
+const Main = () => {
+    return (
+        <main className='main-container'>
+            <h1>Soooooome Heading</h1>
+            <p>some posts</p>
+        </main>
+    );
+}
+
+export default Main;
